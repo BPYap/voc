b4431143d8eb363d80870cee04d022b2a14191cc
BPYap <32807097+BPYap@users.noreply.github.com>
2018-05-14 16:48:49 +0800

    Added coroutine wrapper skeleton

--- python/common/org/python/types/Coroutine.java
+++ python/common/org/python/types/Coroutine.java
@@ -2,6 +2,16 @@ package org.python.types;
 
 public class Coroutine extends org.python.types.Object {
     public org.python.types.Function func;
+
+    private static boolean _DEBUG = false;
+
+    public Coroutine() {
+        func = null;
+    }
+
+    /**
+     * Constructor for coroutine object defined via "async def" syntax.
+     */
     public Coroutine(org.python.types.Function func)
     {
         org.python.types.Int co_flags = func.code.co_flags;
@@ -32,12 +42,78 @@ public class Coroutine extends org.python.types.Object {
         }
         else {
             // wrap function that returns a coroutine or generator object
-
             // TODO: code to wrap the function
         }
 
         this.func = func;
     }
+
+    /**
+     * Implementation of coroutine decorator from asyncio.coroutines.py
+     * Another way to define coroutine
+     * Simply decorates a generator function with "@asyncio.coroutine"
+     */
+    public static org.python.types.Coroutine coroutine(org.python.types.Function func) {
+        org.python.types.Coroutine coro;
+
+        if (_iscoroutinefunction(func)) {
+            coro = new Coroutine();
+            coro.func = func;
+            return coro;
+        }
+
+        if (!_isgeneratorfunction(func)) {
+            // TODO: code to wrap func to support Awaitable or Future object
+        }
+
+        if (!_DEBUG) {
+            coro = new Coroutine(func);
+        }
+        else {
+            // TODO: create debug version of coroutine
+        }
+
+        coro.__setattr__("_is_coroutine", org.python.types.Bool.TRUE);
+        return coro;
+    }
+
+    /**
+     * Implementation of isgeneratorfunction(object) from inspect.py
+     * Return true if the object is a user-defined generator function.
+     */
+    private static boolean _isgeneratorfunction(org.python.types.Object object) {
+        org.python.types.Function func;
+        if (object instanceof  org.python.types.Function) {
+            func = (org.python.types.Function)object;
+        }
+        else if (object instanceof org.python.types.Method) {
+            func = ((org.python.types.Method)object).im_func;
+        }
+        else {
+            return false;
+        }
+        return ((func.code.co_flags.value & Function.CO_GENERATOR) > 0);
+    }
+
+    /**
+     * Implementation of iscoroutinefunction(object) from inspect.py
+     * Return true if the object is a coroutine function.
+     * Coroutine functions are defined with "async def" syntax.
+     */
+    private static boolean _iscoroutinefunction(org.python.types.Object object) {
+        org.python.types.Function func;
+        if (object instanceof  org.python.types.Function) {
+            func = (org.python.types.Function)object;
+        }
+        else if (object instanceof org.python.types.Method) {
+            func = ((org.python.types.Method)object).im_func;
+        }
+        else {
+            return false;
+        }
+        return ((func.code.co_flags.value & Function.CO_COROUTINE) > 0);
+    }
+
 
 
 
